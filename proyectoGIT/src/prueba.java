d05b14180e9325194ce277987d5e02a9f19ab390
Alumno <Alumno@P14>
2013-02-26 08:52:44 +0000

    Version 4

--- proyectoGIT/src/prueba.java
+++ proyectoGIT/src/prueba.java
@@ -2,11 +2,17 @@
 public class prueba {
 	String nombre;
 	
+	/**
+	 * Getter para el nombre
+	 */
 	public String getNombre()
 	{
 		return this.nombre;
 	}
-	// loa que se	qw fq
+	
+	/**
+	 * Setter para el nombre 
+	 */
 	public void setNombre(String nombre)
 	{
 		this.nombre = nombre;
