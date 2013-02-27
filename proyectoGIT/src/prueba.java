db6cdbd289f647ef6456e4017cb0b120cb08d013
mariodpl <mario1989_@hotmail.com>
2013-02-27 10:40:43 +0000

    Version 10

--- proyectoGIT/src/prueba.java
+++ proyectoGIT/src/prueba.java
@@ -2,7 +2,14 @@
 public class prueba {
 	String nombre;
 	String apellido;
-	String apellido2;
+	int edad;
+	
+	public prueba(String nombre, String apellido, int edad)
+	{
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
 	
 	/**
 	 * Getter para el nombre
@@ -21,6 +28,14 @@ public class prueba {
 	}
 	
 	/**
+	 * Getter para la edad
+	 */
+	public int getEdad()
+	{
+		return this.edad;
+	}
+	
+	/**
 	 * Setter para el nombre 
 	 */
 	public void setNombre(String nombre)
@@ -34,5 +49,13 @@ public class prueba {
 	public void setApellido(String apellido)
 	{
 		this.apellido = apellido;
+	}
+	
+	/**
+	 * Setter para la edad
+	 */
+	public void setEdad(int edad)
+	{
+		this.edad = edad;
 	}
 }
